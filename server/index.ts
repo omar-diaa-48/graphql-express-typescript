da3dc0c4c120215a59c0e78a2f848e1e5cde0200
Omar <omar@kamelizer.com>
2021-12-30 11:14:30 +0200

    connect to mongodb

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express()
+
+const MONGO_URI = 'mongodb://127.0.0.1:27017/graphql-demo';
+
+mongoose.connect(MONGO_URI)
+
+mongoose.connection
+    .once('open', () => console.log("Connected to mongodb"))
+    .on('error', (error) => console.log("Error on connecting to mongodb", error))
 
 export default app;
