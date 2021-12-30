8e07ae2ac16cab271b48075b28dd34aefa412837
Omar <omar@kamelizer.com>
2021-12-30 11:46:33 +0200

    initialize passport

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import {graphqlHTTP} from 'express-graphql';
+import schema from "./schema/schema";
+import passport from "passport";
+import session from "express-session";
+
 
 const app = express()
 
@@ -10,5 +15,22 @@ mongoose.connect(MONGO_URI)
 mongoose.connection
     .once('open', () => console.log("Connected to mongodb"))
     .on('error', (error) => console.log("Error on connecting to mongodb", error))
+
+app.use(session({
+    resave:true,
+    saveUninitialized:true,
+    secret:'tm4yz9qw',
+    cookie:{
+        secure:true
+    }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql:true
+}))
 
 export default app;
