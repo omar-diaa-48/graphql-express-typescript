1e2447d3daa63018a103d51bf18f796af0aaf980
Omar <omar@kamelizer.com>
2021-12-30 11:30:11 +0200

    add user model

---
+++ server/models/user.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true,
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String
+    }
+})
+
+UserSchema.pre('save', function(next){
+    const user = this;
+    if(!user.isModified('password')){
+        return next();
+    }
+
+    bcrypt.genSalt(10, function(err, salt:string) {
+        bcrypt.hash(user.password, salt, function(err, hash:string) {
+            if(err){
+                return next(err)
+            }
+
+            user.password = hash;
+            next();
+        });
+    });
+})
+
+UserSchema.methods.comparePassword = function(password, cb){
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        cb(err, isMatch)
+    })
+}
+
+export default mongoose.model('user', UserSchema);
